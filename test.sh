f52491031476ffa670ac3778ceb48b01e134b8b6
sdkattar <57705342+sdkattar@users.noreply.github.com>
2019-11-18 23:09:06 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 npm install
 
-if [$(sudo docker container ls -q --filter name=myserver1_c) != '' ]; then
-sudo docker container stop myserver1_c
-sudo docker container rm myserver1_c --force
+if [$( docker container ls -q --filter name=myserver1_c) != '' ]; then
+ docker container stop myserver1_c
+ docker container rm myserver1_c --force
 fi
 
-if [$(sudo docker image ls -q --filter reference=mydb)!='']; then
-sudo docker image rm myserver1_c
+if [$( docker image ls -q --filter reference=mydb)!='']; then
+ docker image rm myserver1_c
 fi
 
-sudo docker image build -t myserver_img .
+ docker image build -t myserver_img .
 
-sudo docker container run -itd -p 4400:4000 --name myserver1_c myserver_img
+ docker container run -itd -p 4400:4000 --name myserver1_c myserver_img
